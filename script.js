9f4ee1566981b84db41c7bf3b1628803b7ecc531
Vincent-CLY <vchku413@connect.hku.hk>
2023-07-14 12:01:18 +0800

    Modifying The Section Header

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const navContainer = document.querySelector('.nav_container');
 const navIcon = document.querySelector('.nav_icon');
 const textContent = document.querySelectorAll('.text_content');
+const sectionHeader = document.querySelector('.section_header');
+const headerTextCount = sectionHeader.innerText.split('\n').join('').length;
+const r = document.querySelector(':root');
+const button = document.querySelector('button');
+
 
 // Able To Click Menu 
 navIcon.addEventListener('click', () => {
@@ -14,29 +19,19 @@ navIcon.addEventListener('click', () => {
 // Able To Click Away The Menu
 document.addEventListener('click', e => {
     if (!navIcon.contains(e.target)) {
-        navContainer.classList.remove('active');
-        navIcon.classList.remove('active');
-        if (textContent.classList.contains('active')) {
+        if (navContainer.classList.contains('active')) {
             for (let eachTextContent of textContent) {
-                eachTextContent.classList.toggle('active');
+                eachTextContent.classList.remove('active');
             }
         }
+        navContainer.classList.remove('active');
+        navIcon.classList.remove('active');
     }
 })
-
-// q: Can you suggest some of the mordern color palettes for the website? 
-// a: https://www.canva.com/colors/color-palettes/
-// a: https://colorhunt.co/
-// a: https://coolors.co/
-// a: https://color.adobe.com/create/color-wheel
-// q: Can you suggest some of the mordern font pairings for the website?
-// a: https://www.canva.com/font-combinations/
-// a: https://fontjoy.com/
-// a: https://www.typewolf.com/google-fonts
-// a: https://www.fontsquirrel.com/
-// a: https://fonts.google.com/
-// q: I have chose to use #201A1E, #D0B49F, #E4D4C8, and #E2E6E7 for my web coloring using in canva.com. Can you help to decide which color to use in webpage background, div bakground, text color, and navigation bar color?
-// a: #201A1E for webpage background
-// a: #D0B49F for div background
-// a: #E4D4C8 for text color
-// a: #E2E6E7 for navigation bar color
+console.log('Before')
+// Set steps() number to CSS animation
+function testing() {
+    r.style.setProperty('--header-text-count', headerTextCount);
+    console.log(headerTextCount);
+}
+console.log('After')
