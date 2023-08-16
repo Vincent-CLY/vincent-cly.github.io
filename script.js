a515da8c5c15c9c24f40d51828f8f486e290263d
Vincent-CLY <vincent-cly.gamil.com>
2023-08-16 15:08:52 +0800

    Added icon for hobbies and changed form layout

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // Change typing-cursor-effect text
 const toolkit = document.querySelector('[data-toolkit]');
-const toolkitArr = ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Regex', 'Python', 'C++', 'PHP', 'SQL'];
+const toolkitArr = ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Regex', 'Python', 'C++', 'PHP', 'SQL', 'AJAX'];
 let i = 1;
 toolkit.innerText = toolkitArr[0];
 toolkit.style.setProperty('--header-text-count', toolkit.innerText.length);
 
 toolkit.addEventListener('animationiteration', e => {
     if (e.animationName === 'toolkit-typing' && e.pseudoElement === '::after') {
-        toolkit.innerText = languagesArr[i];
+        toolkit.innerText = toolkitArr[i];
         // Set steps() number to CSS animation
         toolkit.style.setProperty('--header-text-count', toolkit.innerText.length);
         i++;
-        if (i === languagesArr.length) {
+        if (i === toolkitArr.length) {
             i = 0;
         }
     }
@@ -27,3 +27,35 @@ window.addEventListener('resize', () => {
     // Set hello svg size
     helloSvg.style.transform = `scale(${scale})`;
 });
+
+// Contact form textarea auto expand
+const message = document.querySelector('#message');
+message.addEventListener('input', e => {
+    message.style.height = '30px';
+    message.style.height = `${message.scrollHeight-3}px`;
+});
+
+// Set contact form icon transform
+const contactInputs = document.querySelectorAll('[data-form]');
+const iconWrappers = document.querySelectorAll('.icon_wrapper');
+
+function setIconTransform(index) {
+    if (contactInputs[index].value.length > 0) {
+        iconWrappers[index].style.transform = 'translateY(-30px) scale(0.9)';
+    }
+    else {
+        iconWrappers[index].style.transform = 'translateY(0px) scale(1)';
+    };
+};
+contactInputs.forEach(function (inputField, index) {
+    inputField.addEventListener('focus', () => iconWrappers[index].style.transform = 'translateY(-30px) scale(0.9)');
+    inputField.addEventListener('blur', () => setIconTransform(index));
+});
+
+// Validate input email
+function emailValidate() {
+    const email = document.querySelector('#email');
+    if (!email.value.match(/(^\w[\w-]+)@([\w-]+\.)+([a-z]{2,4})/g)) {
+        
+    }
+}
